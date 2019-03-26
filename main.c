cbb58d258e4a28feb0d60d52ee3248b4883f12ef
okopel <okopel@gmail.com>
2019-03-26 02:11:28 -0700

    v1.0

--- main.c
+++ main.c
@@ -7,15 +7,23 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 
 #define MAX_CHARS 20
 
 int main() {
     char task[MAX_CHARS];
-    printf("enter task");
-
-    int execl(const char *path, const char *arg, ...);
+    printf("enter task\n");
     scanf("%s", &task);
+    system(task);
+
+
+    printf("end\n");
+
+
+//    scanf("%s", &task);
 
     return 0;
 }
