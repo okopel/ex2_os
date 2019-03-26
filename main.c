605982bb948cd4645dc0acc6625466f6f4679f29
okopel <okopel@gmail.com>
2019-03-26 11:51:36 -0700

    v1.1

--- main.c
+++ main.c
@@ -9,21 +9,93 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 #define MAX_CHARS 20
+#define MAX_WORD 20
+#define DELIMITER " "
+void runShell();
 
-int main() {
-    char task[MAX_CHARS];
-    printf("enter task\n");
-    scanf("%s", &task);
-    system(task);
+char *getInput();
 
+char **getLexer(char *input);
 
-    printf("end\n");
+bool sendToExe(char **lexered);
 
-
-//    scanf("%s", &task);
-
+int main(int argc, char **argv) {
+    runShell();
     return 0;
+}
+
+
+void runShell() {
+
+    char *input;
+    char **lexered;
+    bool hasNext = true;
+    while (hasNext) {
+        input = getInput();
+        lexered = getLexer(input);
+        hasNext = sendToExe(lexered);
+        free(input);
+        free(lexered);
+    }
+
+
+}
+
+char *getInput() {
+    char *input = malloc(sizeof(char) * MAX_CHARS);
+    int maxLen = MAX_CHARS;
+    if (!input) {
+        perror("BAD MALLOC");
+    }
+    char c;
+    int pos = 0;
+    while (true) {
+        c = getchar();
+        if (c == '\n' || c == EOF) {
+            input[pos] = '\0';
+            return input;
+        } else {
+            input[pos] = c;
+            pos++;
+        }
+        if (pos == maxLen) {
+            maxLen += MAX_CHARS;
+            input = realloc(input, maxLen);
+            if (!input) {
+                perror("BAD REALLOC");
+            }
+        }
+    }
+}
+
+
+char **getLexer(char *input) {
+    char **lexered = malloc(sizeof(char) * MAX_WORD);
+    if(!lexered){
+        perror("BAD MALLOC IN LEXER");
+    }
+    int maxWord=MAX_WORD;
+    int pos=0;
+    char *tmp = strtok(input, DELIMITER);
+    while (tmp) {
+        lexered[pos]=tmp;
+        pos++;
+        if(pos==maxWord){
+            maxWord+=MAX_WORD;
+            lexered= realloc(lexered,maxWord);
+            if(!lexered){
+                perror("BAD REALLOC IN LEXER");
+            }
+        }
+        tmp = strtok(input, DELIMITER);
+    }
+    return lexered;
+}
+
+bool sendToExe(char **lexered) {
+
 }
