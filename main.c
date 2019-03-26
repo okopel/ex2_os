323bb08034e69ebe2d4dba0d232ab9ece368fe52
okopel <okopel@gmail.com>
2019-03-26 02:06:36 -0700

    v1.0

---
+++ main.c
@@ -0,0 +1,21 @@
+/**
+ * Ex2
+ * Ori Kopel
+ * 205533151
+ */
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_CHARS 20
+
+int main() {
+    char task[MAX_CHARS];
+    printf("enter task");
+
+    int execl(const char *path, const char *arg, ...);
+    scanf("%s", &task);
+
+    return 0;
+}
